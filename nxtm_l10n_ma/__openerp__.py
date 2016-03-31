5d0f3c4c74a49885381b44ad8d7406740ce08d01
NEXTMA <elkafil@gmail.com>
2016-03-31 11:53:00 +0100

    Update __openerp__.py

--- nxtm_l10n_ma/__openerp__.py
+++ nxtm_l10n_ma/__openerp__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (c) 2010 kazacube (http://kazacube.com).
+#    Copyright (c) 2010 kazacube (http://kazacube.com). 2016 nextma (http://www.nextma.com)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
