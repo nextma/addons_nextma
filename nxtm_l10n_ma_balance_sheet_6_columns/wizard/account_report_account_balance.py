e3e231857043e3c26a997feef0c84d1a29f945f8
NEXTMA <elkafil@gmail.com>
2016-03-31 14:19:02 +0100

    Added files via upload

---
+++ nxtm_l10n_ma_balance_sheet_6_columns/wizard/account_report_account_balance.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import api
+from openerp.osv import fields, osv
+
+
+class account_balance_report(osv.osv_memory):
+    _inherit = 'account.balance.report'
+
+    _columns = {
+        'fiscalyear_id_old': fields.many2one('account.fiscalyear', 'Exercice fiscal (N-1)', help='Keep empty for all open fiscal year'),
+    }
+    def check_report(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        data = {}
+        data['ids'] = context.get('active_ids', [])
+        data['model'] = context.get('active_model', 'ir.ui.menu')
+        data['form'] = self.read(cr, uid, ids, ['date_from',  'date_to',  'fiscalyear_id', 'fiscalyear_id_old', 'journal_ids', 'period_from', 'period_to',  'filter',  'chart_account_id', 'target_move'], context=context)[0]
+        for field in ['fiscalyear_id', 'fiscalyear_id_old', 'chart_account_id', 'period_from', 'period_to']:
+            if isinstance(data['form'][field], tuple):
+                data['form'][field] = data['form'][field][0]
+        used_context = self._build_contexts(cr, uid, ids, data, context=context)
+        data['form']['periods'] = used_context.get('periods', False) and used_context['periods'] or []
+        data['form']['used_context'] = dict(used_context, lang=context.get('lang', 'en_US'))
+        return self._print_report(cr, uid, ids, data, context=context)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
