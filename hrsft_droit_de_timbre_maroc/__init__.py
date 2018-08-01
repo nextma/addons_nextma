420c7a152a7187d1a863a10037a36d2651b3577c
NEXTMA <elkafil@gmail.com>
2018-08-01 22:23:26 +0100

    Add files via upload

---
+++ hrsft_droit_de_timbre_maroc/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Humanytek - Manuel Marquez <manuel@humanytek.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from . import models
