77e89bbdc96ec2c2649e3d7bf5570d8e6e283292
NEXTMA <elkafil@gmail.com>
2018-08-01 22:38:58 +0100

    Add files via upload

---
+++ hrsft_droit_de_timbre_maroc/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 HORIYASOFT <info@horiyasoft.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Droit de Timbre sur les Factures Maroc ',
+    'version': '10.0',
+    'category': 'account',
+    'author': 'HORIYASOFT',
+    'website': "http://www.horiyasoft.com",
+    'summary': """Gestion du droit de Timbre sur les Factures payee en cash""",
+    'license': 'AGPL-3',
+    'description': """
+       
+        Module spécifique pour la gestion du droit de Timbre sur les Factures payee en cash selon la legislation Marocaine
+    """,
+
+    'depends': [
+        'account'
+        ],
+    'data': [
+        'views/stock_picking_view.xml',
+    ],
+    'images': ['static/description/banner.jpeg'],
+    'installable': True,
+    'auto_install': False
+}
+
+
+
