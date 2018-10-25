c71bd4b1ebcdd98a056819457c52f409fc91ce61
NEXTMA <elkafil@gmail.com>
2018-10-25 16:52:48 +0100

    Add files via upload

---
+++ hrsft_ice_unique/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+{
+    'name': " Identifiant Commun de l'Entreprise unique ",
+    'version': '1.0',
+    'author': 'HORIYASOFT',
+    'category': 'Extra Tools',
+    'summary': """Fiche partenaire Maroc""",
+    'license': 'AGPL-3',
+    'website': 'www.horiyasoft.com',
+    'description': """verifie si l'Identifiant Commun de l'Entreprise(ICE) est unique  """,
+    'depends': ["hrsft_clients_maroc"],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+}
