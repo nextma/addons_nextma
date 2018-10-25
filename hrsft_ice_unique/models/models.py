109579cb94de86df901e6bf40d3531a25bcaeece
NEXTMA <elkafil@gmail.com>
2018-10-25 16:52:24 +0100

    Add files via upload

---
+++ hrsft_ice_unique/models/models.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, api
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    @api.multi
+    def copy(self, default=None):  # pylint: disable=W0622
+        if not default:
+            default = {}
+        default['hrsft_ice'] = ""
+        return super(ResPartner, self).copy(default=default)
+
+    _sql_constraints = [
+        ('hrsft_ice_unique', 'UNIQUE(hrsft_ice)', 'Le champ ICE est deja existant !'),
+    ]
