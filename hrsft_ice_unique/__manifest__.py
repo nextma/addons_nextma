9934238b2de366b0b8eb300be0f5179b1388dc0d
NEXTMA <elkafil@gmail.com>
2018-10-25 16:37:13 +0100

    Add files via upload

---
+++ hrsft_ice_unique/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Identifiant Commun de l'Entreprise unique ",
+    'version': '1.0',
+    'author': 'HORIYASOFT',
+    'category': 'Management',
+    'license': 'AGPL-3',
+    'website': 'www.horiyasoft.com',
+    'description': """Rend l'Identifiant Commun de l'Entreprise unique  """,
+    'depends': ["hrsft_clients_maroc"],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+}
