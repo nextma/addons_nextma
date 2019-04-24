361ca0d26a3c301361cc00a7c1a9a10823dccf25
NEXTMA <elkafil@gmail.com>
2019-04-24 12:45:34 +0100

    Add files via upload

---
+++ hrsft_mode_paiement/controllers/controllers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+# class HrsftModePaiement(http.Controller):
+#     @http.route('/hrsft_mode_paiement/hrsft_mode_paiement/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/hrsft_mode_paiement/hrsft_mode_paiement/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('hrsft_mode_paiement.listing', {
+#             'root': '/hrsft_mode_paiement/hrsft_mode_paiement',
+#             'objects': http.request.env['hrsft_mode_paiement.hrsft_mode_paiement'].search([]),
+#         })
+
+#     @http.route('/hrsft_mode_paiement/hrsft_mode_paiement/objects/<model("hrsft_mode_paiement.hrsft_mode_paiement"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('hrsft_mode_paiement.object', {
+#             'object': obj
+#         })
